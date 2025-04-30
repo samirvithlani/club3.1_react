45f64236854e1a416a260da17724283398ece2ed
samirvithlani <samir.vithlani83955@gmail.com>
2025-04-30 16:51:51 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//component..
+function App() {
+  
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <h1>AT A TIME WE CAN RETURN 1 tag only...</h1>
+      <p>EVERY TAG MUST HAVE CLOSING TAG</p>
+      <p>at a time 1 tag must return</p>
+    </div>
+  )
+}
+
+export default App
