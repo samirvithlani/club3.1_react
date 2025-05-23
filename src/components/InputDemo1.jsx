45e8525b6b6189da4906cd4f8f0cbd9147d02bd8
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-23 17:31:12 +0530

    input date

--- src/components/InputDemo1.jsx
+++ src/components/InputDemo1.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 export const InputDemo1 = () => {
     const [name, setname] = useState("")
     const [age, setage] = useState()
+    const [color, setcolor] = useState("black")
+    const [date, setdate] = useState()
     const [isSubbmited, setisSubbmited] = useState(false)
 
     const nameHandler = (event)=>{
@@ -30,13 +32,23 @@ export const InputDemo1 = () => {
             {/* {age} */}
         </div>
         <div>
+            <label>COLOR</label>
+            <input type='color' onChange={(event)=>{setcolor(event.target.value)}}></input>
+        </div>
+        <div>
+            <label>DATE</label>
+            <input type='date' onChange={(event)=>{setdate(event.target.value)}}></input>
+        </div>
+        <div>
             <button onClick={()=>{submitHandler()}}>ADD</button>
         </div>
         {
-            isSubbmited == true &&  <div>
+            isSubbmited == true &&  <div style={{color:color}}>
             <h1>OUTPUT</h1>
             <h1>NAME = {name}</h1>
             <h2>AGE ={age}</h2>
+            <h3>COLOR = {color}</h3>
+            <h4>DATE = {date}</h4>
         </div>
         }
         
